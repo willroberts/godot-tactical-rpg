4954f5f7fbd641aacde074bac59b1c9ec614b71b
Will Roberts <willroberts3@protonmail.com>
2023-08-02 15:26:33 +0200

    Adds comments

--- Scenes/Levels/Level1.cs
+++ Scenes/Levels/Level1.cs
@@ -6,6 +6,13 @@ public partial class Level1 : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Add collision metadata.
+		// TBD.
+
+		// Spawn the player.
+		// TBD.
+
+		// Add enemies.
 		GetNode<Grid>("Grid").SpawnEnemy(new Vector2(8, 6));
 		GetNode<Grid>("Grid").SpawnEnemy(new Vector2(8, 15));
 		GetNode<Grid>("Grid").SpawnEnemy(new Vector2(2, 17));
